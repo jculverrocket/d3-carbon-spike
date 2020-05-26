e69844d6064aabece9dc9eb0fca4d50a7e7949f5
Jessica Culver <jculver@US-L-1022.local>
2020-05-26 16:13:14 -0600

    added a d3 example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LineGraph from './components/LineGraph'
-import DonutGraph from './components/DonutChart'
+import LineGraph from './components/carbon/LineGraph'
+import DonutGraph from './components/carbon/DonutChart'
+import BarChart from './components/d3/BarChart'
+
+const addColSize = (componentName) => {
+  return(
+    <div class="row py-5">
+    <div class='col'></div>
+    <div class='col'>{componentName}</div>
+    <div class='col'></div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <div>
-        <div class='pt-5'>D3 Demo</div>
-        <div class="row pt-5">
-          <div class="col">
-            <LineGraph/>
-          </div>
-          <div class="col">
-            One of three columns
-          </div>
-          <div class="col">
-          <DonutGraph/>
-          </div>
+        <h1 class='py-5'>D3 Demo</h1>
+        <div>
+          <h2>Carbon D3 Line Chart</h2>
+          {addColSize(<LineGraph/>)}
         </div>
+        <div>
+          <h2>Carbon D3 Donut Chart</h2>
+          {addColSize(<DonutGraph/>)}
+        </div>
+        <h2>D3.js BarChart</h2>
+       {addColSize(<BarChart/>)}
       </div>
     </div>
   );
